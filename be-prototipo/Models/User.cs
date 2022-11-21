55d116ff2170039537224828ab382c8232f79749
JJB Personal <04juanjoseb@gmail.com>
2022-11-20 22:14:56 -0500

    Add project files.

---
+++ be-prototipo/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace be_prototipo.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Fullname { get; set; }
+        public string? Genre { get; set; }
+        public int? Age { get; set; }
+
+        public string? Email { get; set; }
+        public string? MobileNumber { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+    }
+}
